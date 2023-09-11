effd33565bce097e2b98398852f92bcad8b7a429
Dewi Louhenapessy <dewilouhenapessy@gmail.com>
2023-09-11 15:14:04 +0200

    added logic for the one meme

--- game/logic.ts
+++ game/logic.ts
@@ -2,40 +2,48 @@ import { RandomMemes } from "@/components/Game";
 import memes from "@/pages/data/memes.json";
 import { useState } from "react";
 
-export const randomMeme = () => {
-  // const [memes, setMemes] = useState([]);
+export const randomMemes = () => {
+	// const [memes, setMemes] = useState([]);
 
-  const shuffledMemes = memes.sort(() => 0.5 - Math.random()); // Shuffle the memes
-  const firstThreeMemes = shuffledMemes.slice(0, 3); // Get the first three memes
-  // setMemes(firstThreeMemes);
-  // console.log(firstThreeMemes);
+	const shuffledMemes = memes.sort(() => 0.5 - Math.random()); // Shuffle the memes
+	const firstThreeMemes = shuffledMemes.slice(0, 3); // Get the first three memes
+	// setMemes(firstThreeMemes);
+	// console.log(firstThreeMemes);
 
-  return firstThreeMemes;
+	return firstThreeMemes;
+};
+randomMemes();
+
+const answerMeme = () => {
+	const randomMemesArray = randomMemes();
+	const randomMeme =
+		randomMemesArray[Math.floor(Math.random() * randomMemesArray.length)];
+	console.log(randomMeme);
 };
 
-console.log(randomMeme());
+answerMeme();
 
 // util for easy adding logs
 const addLog = (message: string, logs: GameState["log"]): GameState["log"] => {
-  return [{ dt: new Date().getTime(), message: message }, ...logs].slice(
-    0,
-    MAX_LOG_SIZE
-  );
+	return [{ dt: new Date().getTime(), message: message }, ...logs].slice(
+		0,
+		MAX_LOG_SIZE
+	);
 };
 
 // If there is anything you want to track for a specific user, change this interface
 export interface User {
-  id: string;
+	id: string;
 }
 
 // Do not change this! Every game has a list of users and log of actions
 interface BaseGameState {
-  memes: RandomMemes[];
-  users: User[];
-  log: {
-    dt: number;
-    message: string;
-  }[];
+	memes: RandomMemes[];
+	users: User[];
+	log: {
+		dt: number;
+		message: string;
+	}[];
 }
 
 // Do not change!
@@ -53,71 +61,70 @@ export type DefaultAction = { type: "UserEntered" } | { type: "UserExit" };
 
 // This interface holds all the information about your game
 export interface GameState extends BaseGameState {
-  target: number;
+	target: number;
 }
 
 // This is how a fresh new game starts out, it's a function so you can make it dynamic!
 // In the case of the guesser game we start out with a random target
 export const initialGame = () => ({
-  // const [randomMeme, setRandomMemes] = useState([]),
-  memes: randomMeme(),
+	// const [randomMeme, setRandomMemes] = useState([]),
+	memes: randomMemes(),
 
-  // const shuffledMemes = memes.sort(() => 0.5 - Math.random()); // Shuffle the memes
-  // const firstThreeMemes = shuffledMemes.slice(0, 3); // Get the first three memes
-  // setMemes(firstThreeMemes)
+	// const shuffledMemes = memes.sort(() => 0.5 - Math.random()); // Shuffle the memes
+	// const firstThreeMemes = shuffledMemes.slice(0, 3); // Get the first three memes
+	// setMemes(firstThreeMemes)
 
-  users: [],
-  // target:
-  target: Math.floor(Math.random() * 2),
-  log: addLog("🐄 Game Created!", []),
+	users: [],
+	target: answerMeme(),
+	log: addLog("🐄 Game Created!", []),
 });
 
 // Here are all the actions we can dispatch for a user
 type GameAction = { type: "guess"; guess: number };
 
 export const gameUpdater = (
-  action: ServerAction,
-  state: GameState
+	action: ServerAction,
+	state: GameState
 ): GameState => {
-  // This switch should have a case for every action type you add.
+	// This switch should have a case for every action type you add.
 
-  // "UserEntered" & "UserExit" are defined by default
+	// "UserEntered" & "UserExit" are defined by default
 
-  // Every action has a user field that represent the user who dispatched the action,
-  // you don't need to add this yourself
-  switch (action.type) {
-    case "UserEntered":
-      return {
-        ...state,
-        users: [...state.users, action.user],
-        log: addLog(`user ${action.user.id} joined 🎉`, state.log),
-      };
+	// Every action has a user field that represent the user who dispatched the action,
+	// you don't need to add this yourself
+	switch (action.type) {
+		case "UserEntered":
+			return {
+				...state,
+				users: [...state.users, action.user],
+				log: addLog(`user ${action.user.id} joined 🎉`, state.log),
+			};
 
-    case "UserExit":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.user.id),
-        log: addLog(`user ${action.user.id} left 😢`, state.log),
-      };
+		case "UserExit":
+			return {
+				...state,
+				users: state.users.filter((user) => user.id !== action.user.id),
+				log: addLog(`user ${action.user.id} left 😢`, state.log),
+			};
 
-    case "guess":
-      if (action.guess === state.target) {
-        return {
-          ...state,
-          target: Math.floor(Math.random() * 2),
-          log: addLog(
-            `user ${action.user.id} answered ${action.guess} and won! 👑`,
-            state.log
-          ),
-        };
-      } else {
-        return {
-          ...state,
-          log: addLog(
-            `user ${action.user.id} answered ${action.guess}`,
-            state.log
-          ),
-        };
-      }
-  }
+		case "guess":
+			if (action.guess === state.target) {
+				return {
+					...state,
+					target: Math.floor(Math.random() * 2),
+					log: addLog(
+						`user ${action.user.id} answered ${action.guess} and won! 👑`,
+						state.log
+					),
+				};
+			} else {
+				return {
+					...state,
+					log: addLog(
+						`user ${action.user.id} answered ${action.guess}`,
+						state.log
+					),
+				};
+			}
+	}
 };
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,158 +1,158 @@
 import { useEffect, useState } from "react";
 import { useGameRoom } from "@/hooks/useGameRoom";
-import { randomMeme } from "../../game/logic";
+import { randomMemes } from "../../game/logic";
 
 interface GameProps {
-  username: string;
-  roomId: string;
+	username: string;
+	roomId: string;
 }
 
 export interface RandomMemes {
-  id: string;
-  name: string;
-  url: string;
-  width?: number;
-  height?: number;
-  box_count?: number;
-  captions?: number;
+	id: string;
+	name: string;
+	url: string;
+	width?: number;
+	height?: number;
+	box_count?: number;
+	captions?: number;
 }
 
 const Game = ({ username, roomId }: GameProps) => {
-  const { gameState, dispatch } = useGameRoom(username, roomId);
-  console.log(gameState);
-  // Local state to use for the UI
-  const [guess, setGuess] = useState<number>(0);
+	const { gameState, dispatch } = useGameRoom(username, roomId);
+	console.log(gameState);
+	// Local state to use for the UI
+	const [guess, setGuess] = useState<number>(0);
 
-  // const [randomMemes, setRandomMemes] = useState<RandomMemes[] | []>([]);
+	// const [randomMemes, setRandomMemes] = useState<RandomMemes[] | []>([]);
 
-  // useEffect(() => {
-  //   const random = randomMeme();
-  //   if (random) {
-  //     setRandomMemes(random);
-  //   } else {
-  //     return;
-  //   }
-  // }, []);
+	// useEffect(() => {
+	//   const random = randomMeme();
+	//   if (random) {
+	//     setRandomMemes(random);
+	//   } else {
+	//     return;
+	//   }
+	// }, []);
 
-  // Indicated that the game is loading
-  if (gameState === null) {
-    return (
-      <p>
-        <span className="transition-all w-fit inline-block mr-4 animate-bounce">
-          🎲
-        </span>
-        Waiting for server...
-      </p>
-    );
-  }
+	// Indicated that the game is loading
+	if (gameState === null) {
+		return (
+			<p>
+				<span className="transition-all w-fit inline-block mr-4 animate-bounce">
+					🎲
+				</span>
+				Waiting for server...
+			</p>
+		);
+	}
 
-  const handleGuess = (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    // Dispatch allows you to send an action!
-    // Modify /game/logic.ts to change what actions you can send
-    dispatch({ type: "guess", guess: guess });
-  };
+	const handleGuess = (event: React.SyntheticEvent) => {
+		event.preventDefault();
+		// Dispatch allows you to send an action!
+		// Modify /game/logic.ts to change what actions you can send
+		dispatch({ type: "guess", guess: guess });
+	};
 
-  return (
-    <>
-      <h1 className="text-2xl border-b border-yellow-400 text-center relative">
-        🎲 Guess the number!
-      </h1>
-      {gameState.memes.map((meme) => {
-        return (
-          <div key={meme.id}>
-            <p>{meme.name}</p>
-            <img src={meme.url} />
-          </div>
-        );
-      })}
+	return (
+		<>
+			<h1 className="text-2xl border-b border-yellow-400 text-center relative">
+				🎲 Guess the number!
+			</h1>
+			{gameState.memes.map((meme) => {
+				return (
+					<div key={meme.id}>
+						<p>{meme.name}</p>
+						<img src={meme.url} />
+					</div>
+				);
+			})}
 
-      <section>
-        <form
-          className="flex flex-col gap-4 py-6 items-center"
-          onSubmit={handleGuess}
-        >
-          <label
-            htmlFor="guess"
-            className="text-7xl font-bold text-stone-50 bg-black rounded p-2 text-"
-          >
-            Random Meme 1
-          </label>
-          <input
-            type="radio"
-            name="guess"
-            id="guess"
-            className="opacity-70 hover:opacity-100 accent-yellow-400"
-            onChange={(e) => setGuess(Number(e.currentTarget.value))}
-            value={guess}
-          />
-          <label
-            htmlFor="guess"
-            className="text-7xl font-bold text-stone-50 bg-black rounded p-2 text-"
-          >
-            Random Meme 2
-          </label>
-          <input
-            type="radio"
-            name="guess"
-            id="guess"
-            className="opacity-70 hover:opacity-100 accent-yellow-400"
-            onChange={(e) => setGuess(Number(e.currentTarget.value))}
-            value={guess}
-          />
-          <label
-            htmlFor="guess"
-            className="text-7xl font-bold text-stone-50 bg-black rounded p-2 text-"
-          >
-            Random Meme 3
-          </label>
-          <input
-            type="radio"
-            name="guess"
-            id="guess"
-            className="opacity-70 hover:opacity-100 accent-yellow-400"
-            onChange={(e) => setGuess(Number(e.currentTarget.value))}
-            value={guess}
-          />
-          <button className="rounded border p-5 bg-yellow-400 group text-black shadow hover:shadow-lg transition-all duration-200 hover:animate-wiggle">
-            Guess!
-          </button>
-        </form>
+			<section>
+				<form
+					className="flex flex-col gap-4 py-6 items-center"
+					onSubmit={handleGuess}
+				>
+					<label
+						htmlFor="guess"
+						className="text-7xl font-bold text-stone-50 bg-black rounded p-2 text-"
+					>
+						Random Meme 1
+					</label>
+					<input
+						type="radio"
+						name="guess"
+						id="guess"
+						className="opacity-70 hover:opacity-100 accent-yellow-400"
+						onChange={(e) => setGuess(Number(e.currentTarget.value))}
+						value={guess}
+					/>
+					<label
+						htmlFor="guess"
+						className="text-7xl font-bold text-stone-50 bg-black rounded p-2 text-"
+					>
+						Random Meme 2
+					</label>
+					<input
+						type="radio"
+						name="guess"
+						id="guess"
+						className="opacity-70 hover:opacity-100 accent-yellow-400"
+						onChange={(e) => setGuess(Number(e.currentTarget.value))}
+						value={guess}
+					/>
+					<label
+						htmlFor="guess"
+						className="text-7xl font-bold text-stone-50 bg-black rounded p-2 text-"
+					>
+						Random Meme 3
+					</label>
+					<input
+						type="radio"
+						name="guess"
+						id="guess"
+						className="opacity-70 hover:opacity-100 accent-yellow-400"
+						onChange={(e) => setGuess(Number(e.currentTarget.value))}
+						value={guess}
+					/>
+					<button className="rounded border p-5 bg-yellow-400 group text-black shadow hover:shadow-lg transition-all duration-200 hover:animate-wiggle">
+						Guess!
+					</button>
+				</form>
 
-        <div className="border-t border-yellow-400 py-2" />
-        {/* <button
+				<div className="border-t border-yellow-400 py-2" />
+				{/* <button
           className="border border-black p-5"
           onClick={() => dispatch({ type: "bet", amount: 100 })}
         >
           Bet!
         </button> */}
 
-        <div className=" bg-yellow-100 flex flex-col p-4 rounded text-sm">
-          {gameState.log.map((logEntry, i) => (
-            <p key={logEntry.dt} className="animate-appear text-black">
-              {logEntry.message}
-            </p>
-          ))}
-        </div>
+				<div className=" bg-yellow-100 flex flex-col p-4 rounded text-sm">
+					{gameState.log.map((logEntry, i) => (
+						<p key={logEntry.dt} className="animate-appear text-black">
+							{logEntry.message}
+						</p>
+					))}
+				</div>
 
-        <h2 className="text-lg">
-          Players in room <span className="font-bold">{roomId}</span>
-        </h2>
-        <div className="flex flex-wrap gap-2">
-          {gameState.users.map((user) => {
-            return (
-              <p
-                className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-black text-white"
-                key={user.id}
-              >
-                {user.id}
-              </p>
-            );
-          })}
-        </div>
-      </section>
-    </>
-  );
+				<h2 className="text-lg">
+					Players in room <span className="font-bold">{roomId}</span>
+				</h2>
+				<div className="flex flex-wrap gap-2">
+					{gameState.users.map((user) => {
+						return (
+							<p
+								className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-black text-white"
+								key={user.id}
+							>
+								{user.id}
+							</p>
+						);
+					})}
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Game;
